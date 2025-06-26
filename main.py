f2760d88892de5c4ccc8726d168ce070c59fde6e
joo9710 <joo9710@naver.com>
2025-06-26 10:19:08 +0900

    CORS URL 추가

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI(lifespan=lifespan)
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://43.203.198.237", "http://localhost"],
+    allow_origins=["http://localhost:5173", "http://43.203.198.237", "http://localhost", "http://localhost:5985"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
